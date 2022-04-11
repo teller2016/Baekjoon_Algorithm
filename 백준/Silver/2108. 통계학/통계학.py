2d92ac6b7d8b670db13444bc5934aafebcc28d58
teller2016 <65855364+teller2016@users.noreply.github.com>
2022-04-11 21:54:19 +0900

    [Silver III] Title: 통계학, Time: 536 ms, Memory: 52628 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.py
+++ 백준/Silver/2108. 통계학/통계학.py
@@ -1,23 +1,29 @@
+import collections
 import sys
-from collections import Counter
 n = int(sys.stdin.readline())
-li = []
+nums = []
 for _ in range(n):
-    li.append(int(sys.stdin.readline()))
- 
-# 산술평균 - 다 더해서 / n
-print(round(sum(li)/n))
- 
-# 중앙값 - 오름차순 -> 중간값
-li.sort()
-print(li[n//2])
- 
-# 최빈값 - 빈출
-cnt_li = Counter(li).most_common()
-if len(cnt_li) > 1 and cnt_li[0][1]==cnt_li[1][1]: #최빈값 2개 이상
-    print(cnt_li[1][0])
-else:
-    print(cnt_li[0][0])
- 
-# 범위 - 최댓값-최솟값
-print(max(li)-min(li))
+    nums.append(int(sys.stdin.readline()))
+
+
+def most(num):
+    counter = collections.Counter(num)
+    most_count = counter.most_common(2)
+    if len(most_count) > 1:
+        if most_count[0][1] == most_count[1][1]:
+            return most_count[1][0]
+        else:
+            return most_count[0][0]
+    else:
+        return most_count[0][0]
+
+
+def solution(nums):
+    nums.sort()
+    print(round(sum(nums) / len(nums)))
+    print(nums[len(nums) // 2])
+    print(most(nums))
+    print(nums[-1] - nums[0])
+
+
+solution(nums)
